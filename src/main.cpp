b74b13d5d6974e7fc53f884ef33159c4f40912e8
unknown <rainbowkun2@gmail.com>
2026-08-10 17:00:03 +0300

    Remove redundant comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-// Dear ImGui: standalone example application for Windows API + DirectX 11
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
 #include "main.h"
 
 // Data
